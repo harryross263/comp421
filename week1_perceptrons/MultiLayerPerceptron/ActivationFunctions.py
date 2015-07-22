ce92207f096cc06d52f1b773fc07755617fed9e0
MASHALLARYAN1 <mashall.aryan@yahoo.come>
2015-07-22 19:15:22 +1200

    comment added

--- week1_perceptrons/MultiLayerPerceptron/ActivationFunctions.py
+++ week1_perceptrons/MultiLayerPerceptron/ActivationFunctions.py
@@ -1,10 +1,11 @@
 
 import numpy as np    
-
       
 def sigmoid(X):
     return 1./(1.+np.exp(-X))    
-
+'''
+derivative of sigmoid function
+'''
 def dSigmoid(X):
     return sigmoid(X)*(1-sigmoid(X))
 
